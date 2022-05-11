cdb6d73d0d3f99646103510baaaef10323afdf51
Gregory <gregorysjs@gmail.com>
2022-05-11 15:13:13 -0300

    2v

--- demo_create_table2.js
+++ demo_create_table2.js
@@ -1,3 +1,4 @@
+
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -10,7 +11,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE users (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "CREATE TABLE users (id int, name VARCHAR(255), favorite_product VARCHAR(255))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
